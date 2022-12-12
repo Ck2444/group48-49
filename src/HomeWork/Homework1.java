e91794bde72e99eeeb660bb9c88c860e8b661b4d
konstantinevstigneev <t777tt88@gmai.com>
2022-12-12 21:13:06 +0100

    Homework11

---
+++ src/HomeWork/Homework1.java
@@ -0,0 +1,35 @@
+package HomeWork;
+
+public class Homework1 {
+    public static void main(String[] args) {
+
+        //2. создать два проекта Java, переключить в настройках один на JDK 11, второй на JDK 17
+
+        byte a = 1;
+        short b = 2;
+        int c = 5;
+        long d = 1000;
+        float e = 4.134556F;
+        double f = 45555.56546667;
+        char g = 'B';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+
+        //3. В методе main инициализировать все примитивные типы и вывести их в консоль
+        int x = 321;
+        int z = x/100;
+        int i = x/10 %10;
+        int r = x%10;
+        System.out.println(z);
+        System.out.println(i);
+        System.out.println(r);
+
+
+    }
+
+}
